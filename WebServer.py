9697fccb32355191ab3a02b1c83714925436580e
Divya Zahra Sabhira <121840114+dzarsh@users.noreply.github.com>
2023-05-07 14:29:47 +0700

    Update WebServer.py

--- WebServer.py
+++ WebServer.py
@@ -1,61 +1,60 @@
 #import socket module
-import socket
+from socket import *
 
 def TCPserver():
     #mengaitkan ke alamat dan port tertentu
-    SERVER_HOST = "localhost"
-    SERVER_PORT = 8080
-
+    serverHost = 'localhost'
+    serverPort = 8080
+    
     #membuat TCP socket
-    sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
     #mendengarkan koneksi dari client
-    sock_server.bind((SERVER_HOST, SERVER_PORT))
-    sock_server.listen()
-
-    print("Server Ready....")
+    serverSocket.bind((serverHost, serverPort))
+    serverSocket.listen()
 
     while True:
         #menerima koneksi dari client
-        sock_client, client_address = sock_server.accept()
-  
+        print("Server Ready....")
+        connectionSocket, addr = serverSocket.accept()
+
         #meminta 
-        request = sock_client.recv(1024).decode()
+        request = connectionSocket.recv(1024).decode()
         print("Dari client: "+request)
 
         #memberi respon 
-        response = handle_request()
-        sock_client.send(response.encode())
+        response = handleRequest()
+        connectionSocket.send(response.encode())
 
-        #menutup koneksi client
-        sock_client.close()
+        #menutup koneksi
+        connectionSocket.close()
 
     #menutup server
-    sock_server.close()
+    serverSocket.close()
     
-def handle_request():
-     try:
-        response_line = "HTTP/1.1 200 OK\r\n"
-        content_type = "Content-type: text/HTML\r\n\r\n"
-        
+def handleRequest():
+    try: 
+        responseLine = "HTTP/1.1 200 OK\r\n"
+        contentType = "Content-type: text/HTML\r\n\r\n"
+
         file = open("docs_HTML/idx.html",'r')
-        message_body = file.read()
+        messageBody = file.read()
         file.close()
-        
-        response = response_line+content_type+message_body
-        return response
-     
-     except:
-        response_line = "HTTP/1.1 404 Not Found\r\n"
-        content_type = "Content-type: text/HTML\r\n\r\n"
-        
-        file = open("docs_HTML/idx1.html",'r')
-        message_body = file.read()
-        file.close()
-        
-        response = response_line+content_type+message_body
+
+        response = responseLine+contentType+messageBody
         return response
 
-if __name__ == "__main__":
+    except IOError:
+        responseLine = "HTTP/1.1 404 Not Found\r\n"
+        contentType = "Content-type: text/HTML\r\n\r\n"
+    
+        file = open("docs_HTML/idx1.html", 'r')
+        messageBody = file.read()
+        file.close()
+
+        response = responseLine+contentType+messageBody
+        return response
+    
+if __name__ == '__main__':
     TCPserver()
